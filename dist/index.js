9f171513628fa2d203f81c80f79a3cb916221274
Florian Richter <floririchte@gmail.com>
2020-07-31 23:20:18 +0200

    Prepare v0.0.11

---
+++ dist/index.js
@@ -0,0 +1,56 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = require("@actions/core");
+const github = require("@actions/github");
+async function run() {
+    try {
+        const key = core.getInput("key");
+        const value = core.getInput("value");
+        const token = core.getInput("token");
+        const octokit = github.getOctokit(token);
+        const { owner, repo } = github.context.repo;
+        const path = `docs/data/${key}.json`;
+        let res = await octokit.repos.getContent({
+            owner,
+            repo,
+            path,
+        });
+        let data = null;
+        const isUpdate = res.status == 200;
+        if (res.status == 404) {
+            core.info(`Found new key "${key}", will create new file.`);
+            data = { key, type: "scalar", values: [] };
+        }
+        else if (isUpdate) {
+            core.info(`Extending existing metrics for "${key}"`);
+            data = JSON.parse(fromBase64(res.data.content));
+        }
+        if (data == null) {
+            throw new Error(`Loading or updating data for "${key}" has failed`);
+        }
+        data.values.push({
+            value: Number.parseFloat(value),
+            isoDate: new Date().toISOString(),
+        });
+        const content = toBase64(JSON.stringify(data));
+        const updateRes = await octokit.repos.createOrUpdateFileContents({
+            owner,
+            repo,
+            path,
+            content,
+            sha: isUpdate ? res.data.sha : undefined,
+            message: isUpdate ? "Updated metrics" : "Created metrics",
+        });
+        core.info("Finished processing new metrics");
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
+function fromBase64(content) {
+    return Buffer.from(content, "base64").toString("ascii");
+}
+function toBase64(content) {
+    return Buffer.from(content).toString("base64");
+}
+run();
