801b7320950fb2c3e54b582478220cc0c77f3285
Florian Richter <floririchte@gmail.com>
2020-07-31 21:39:37 +0200

    Try Octokit

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const github = require("@actions/github");
 try {
   const key = core.getInput("key");
   const key = core.getInput("value");
+  const token = core.getInput("token");
+  const octokit = github.getOctokit(token);
 
-  console.log(`key= ${key}; value=${value}`);
+  const allRepos = await octokit.repos.listPublic();
+
+  console.log(allRepos.data);
 } catch (error) {
   core.setFailed(error.message);
 }
