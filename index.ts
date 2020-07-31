a320206b6cfcbe3b2768919a05fe4878c577a441
Florian Richter <floririchte@gmail.com>
2020-07-31 21:54:48 +0200

    Use Typescript

---
+++ index.ts
@@ -0,0 +1,19 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+async function run() {
+  try {
+    const key = core.getInput("key");
+    const value = core.getInput("value");
+    const token = core.getInput("token");
+    const octokit = github.getOctokit(token);
+
+    const allRepos = await octokit.repos.listPublic();
+
+    core.info(allRepos.data.map((n) => n.name).reduce((a, b) => `${a}, ${b}`));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
