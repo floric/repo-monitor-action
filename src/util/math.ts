64a36dfcaf89faba87a7e1174c0b348bed136cbd
Florian Richter <floririchte@gmail.com>
2020-09-20 15:33:26 +0200

    fix: Fixed statistics

--- src/util/math.ts
+++ src/util/math.ts
@@ -2,18 +2,41 @@ export const roundNumber = (value: number, precision: number) =>
   Math.floor(value * Math.pow(10, precision)) / Math.pow(10, precision);
 
 export const calcMin = (values: Array<number>) =>
-  values.reduce((a, b) => (a < b ? a : b), 0);
+  calculateOrNull(values, () =>
+    values.reduce((a, b) => (a < b ? a : b), 999999999)
+  );
 
 export const calcMax = (values: Array<number>) =>
-  values.reduce((a, b) => (a > b ? a : b), 0);
+  calculateOrNull(values, () =>
+    values.reduce((a, b) => (a > b ? a : b), -999999999)
+  );
 
 export const calcAvg = (values: Array<number>) =>
-  values.reduce((a, b) => a + b, 0) / values.length;
+  calculateOrNull(
+    values,
+    () => values.reduce((a, b) => a + b, 0) / values.length
+  );
 
-export const calcPercentile = (values: Array<number>, percentile: number) => {
-  const sorted = values.sort();
-  const index = values.length * (percentile / 100);
-  const lowerBound = Math.floor(index);
-  const upperBound = Math.ceil(index);
-  return (sorted[lowerBound] + sorted[upperBound]) / 2;
+export const calcPercentile = (
+  values: Array<number>,
+  percentile: number
+): null | number => {
+  return calculateOrNull(values, () => {
+    const sorted = values.sort();
+    const index = values.length * (percentile / 100);
+    const lowerBound = Math.floor(index);
+    const upperBound = Math.ceil(index);
+    return (sorted[lowerBound] + sorted[upperBound]) / 2;
+  });
+};
+
+const calculateOrNull = (
+  values: Array<number>,
+  fn: () => number
+): number | null => {
+  if (!values || values.length === 0) {
+    return null;
+  }
+
+  return fn();
 };
